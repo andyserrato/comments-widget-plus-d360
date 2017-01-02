df827c9e1cf137907676bd34412beb06be90b78d
andyserrato <andyserrato@hotmail.com>
2017-01-02 19:05:55 +0100

    - cambios

---
+++ wp-content/plugins/comments-widget-plus-d360/includes/get_comments.php
@@ -0,0 +1,34 @@
+<?php
+
+$path = $_SERVER['DOCUMENT_ROOT'];
+
+include_once $path . '/wp-config.php';
+include_once $path . '/wp-load.php';
+include_once $path . '/wp-includes/wp-db.php';
+include_once $path . '/wp-includes/pluggable.php';
+    global $wpdb;
+    $query = stripslashes(urldecode($_POST['consulta']));
+
+    $comments = $wpdb->get_results($query);
+    $comentarios = array();
+    foreach ($comments as $comment) {
+        $comentario = new Comentario();
+
+        $comentario->contenido = $comment->comment_content;
+        $comentario->avatar = get_avatar($comment->email, $args['avatar_size']);
+        $comentario->enlace = esc_url(get_comment_link($comment->comment_ID));
+        $comentario->enlace_autor = "../user/" . get_comment_author_link($comment->comment_ID);
+        $comentario->titulo = '<a href="' . $comentario->enlace_autor . '">' . $comment->autor . '</a> en <a href="' . get_post_permalink($comment->post_id) . '">' . get_the_title($comment->post_id) . '</a>';
+        $comentario->enlace_facebook = getLinkFacebookShare($comment);
+        $comentario->enlace_twitter = getLinkTwitterShare($comment);
+        $category_detail = get_the_category($comment->post_id)[0];
+        $comentario->categoria = '<a href="'.esc_url( get_category_link( $category_detail->term_id ) ).'">'.$category_detail->name."</a>";
+        $comentario->categoria_sin_acentos = '<a href="'.remove_accents(strtolower($category_detail->name)).'">'.$category_detail->name."</a>";
+
+        array_push($comentarios, $comentario);
+    }
+
+    echo json_encode($comentarios);
+
+    //echo urldecode($_POST['consulta']);
+?>
